1b5525c5ca67d83df0b3e8867453c507d351387d
suhaswarrior <64247982+suhaswarrior@users.noreply.github.com>
2022-05-18 17:46:10 +0530

    Add files via upload

---
+++ filetype.py
@@ -0,0 +1,4 @@
+'''author : subhajji suhas'''
+file=input("enter the file name with extension ")
+f=file.split(".")
+print("extension of the file is :"+f[-1])
---
+++ largestelement.py
@@ -0,0 +1,7 @@
+''' author :subhajji suhas'''
+list1=[]
+n=int(input("enter the number"))
+for i in range(1,n+1):
+    element=int(input("enter the elements:"))
+    list1.append(element)
+print("largest ele:",max(list1))
