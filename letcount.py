5fe94f4470aa19c4bf599b428939e4e7f4780e1a
suhaswarrior <64247982+suhaswarrior@users.noreply.github.com>
2022-05-25 08:39:13 +0530

    Update letcount.py
    Changes

--- letcount.py
+++ letcount.py
@@ -1,14 +1,4 @@
-def lrtcount(str1):
-    ic = {}
-    for n in str1:
-        keys = ic.keys()
-        if n in keys:
-            ic[n] += 1
-        else:
-            ic[n] = 1
-    return ic  
-str1=str(input("Please enter astring"))
-sol=lrtcount(str1)
-sol.sort()
-print(sol)
-
+from collections import Counter
+test_str =str(input("Please enter astring"))
+res = Counter(test_str) 
+print ("Count of all characters in GeeksforGeeks is :\n " +  str(res))
