1295e3db7cd03503659e9680f3977e5a4d0de97e
suhaswarrior <64247982+suhaswarrior@users.noreply.github.com>
2022-05-30 22:16:05 +0530

    Add files via upload

---
+++ charecter_frequency.py
@@ -0,0 +1,23 @@
+def printChar(arr, Len):
+	occ = {}
+	for i in range(Len):
+		if(arr[i] in occ):
+			occ[arr[i]] = occ[arr[i]] + 1
+		else:
+			occ[arr[i]] = 1
+	size = len(occ)
+	while (size > 0):
+		currentMax = 0
+		arg_max = 0
+		for key, value in occ.items():
+
+			if (value > currentMax or (value == currentMax and key > arg_max)):
+				arg_max = key
+				currentMax = value
+		print(f"{arg_max} - {currentMax}")
+		occ.pop(arg_max)
+		size -= 1
+print("enter a string")
+Str = input()
+Len = len(Str)
+printChar(list(Str), Len)
